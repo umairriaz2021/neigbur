47a4fd35bef0e7c4fa0d0bbf047a622812c0fb0a
Invision-093 <invision093@invisionssolutions.ca>
2021-05-20 18:55:29 +0500

    Change thank you page graphics to match event creation success page graphics

--- wp-content/themes/Divi Child/thank-you.php
+++ wp-content/themes/Divi Child/thank-you.php
@@ -16,30 +16,86 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $result = curl_exec($ch);
 curl_close($ch);
 $apirespons=json_decode($result);
+
+$eventdetail = $apirespons->ticketOrders[0]->event;
+if(isset($eventdetail)){
+    $ch   = curl_init(API_URL.'/events/'.$eventdetail->id);
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+           'Content-Type: application/json',
+           'Authorization: ' . $token
+       ));
+       $result = curl_exec($ch);
+       curl_close($ch);
+       $events =json_decode($result);
+      $event_detail = $events->event;
+
+}
 get_header(); ?>
 
-    <div id="main-content">
-
-
-       <div class="outer-wrapper">
+        <div id="main-content">
+     <div class="outer-wrapper">
            <div class="container container-home">
-      <?php
-           while ( have_posts() ) : the_post(); ?> 
-           <div class="entry-content-page">
-            <?php the_content(); ?>
-           </div>
+              <div class="edit-event" >
+             	<h3 class="h3-title">Thank you for your order. </h3>
+    			 	<div class="event-type">
+                  		<h3>Promote your event now...</h3>
+                        <p> Share your event details on your social media pages</p>
+                    </div>
+                	  <div class="event-detail" style="border-bottom:none !important;">
+                			<div  class="upload-image">
+                			   <img src="https://storage.googleapis.com/<?php echo $event_detail->files[0]->bucket.'/'.$event_detail->files[0]->filename ?>">
+    
+                			</div>
+                            	<div class="event-details">
+                            			
+                            				<h3><?php echo isset($event_detail) ? $event_detail->name : '';?></h3>
+                            			       	<?php foreach($event_detail->event_dates as $edate)
+                                          {
+                                            echo "<b class='p-date'>" . format_dates($edate->start_date, $edate->end_date) . "</b>";
+                                          }
 
-    <?php
-    endwhile;
-    wp_reset_query(); 
-    ?>
-                  
-      <div class="download-link">
+                     if(isset($event_detail)) {
+
+                        $country = $wpdb->get_row("Select * from wp_countries where  id = $event_detail->country_id");
+                        $state = $wpdb->get_row("select * from wp_states where id = $event_detail->province_id");
+                     }
+                     ?>
+                  <p><?php echo isset($event_detail) ? !empty($event_detail->location) ? $event_detail->location.'<br>': ' ' : ' '; ?>
+                     <?php echo isset($event_detail) ? !empty($event_detail->address2) ? $event_detail->address2.'<br>': ' ' : ' '; ?>
+                     <?php echo isset($event_detail) ? $event_detail->city : '';?>, <?php echo isset($event_detail) ? $state->name : '';?>, <?php echo isset($event_detail) ? $country->name : '';?><br/>
+                     <?php echo isset($event_detail) ? $event_detail->postalcode : '';?>
+                  </p>
+                            				<p>
+                            				<!--<button onclick="jQuery('#shareModal').show();">SHARE</button>-->
+                            					<div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?php echo site_url().'/view-event/'.$event_id; ?>" data-a2a-title="<?php echo isset($event_detail) ? $event_detail->name : '';?>">
+                                                    <a class="a2a_button_facebook"></a>
+                                                   <a class="a2a_button_twitter"></a>
+                                                    <a class="a2a_button_pinterest"></a>
+                                                    <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+                                                </div>
+
+                                                <script async src="https://static.addtoany.com/menu/page.js"></script>
+                                          </p>
+                                 </div>
+
+                    	   </div>
+                    	   <div class="events_links" style="margin-bottom:50px;">
+                        				<h3>Your Event address is...</h3>
+
+                        	        	<p class="copy-event-link">
+                        	        	<a id="eveUrl" href="<?php echo site_url(); ?>/view-event/<?php echo $event_detail->id;?>"><?php echo site_url(); ?>/view-event/<?php echo $event_detail->id;?></a>
+                        					<button class="cpy" onclick="copyEventurl('#eveUrl')">COPY</button><span class="copied" style="display:none"></span>
+                        				  </p>
+                        			</div>
+                           <div class="download-link">
         <p class="return-home2"><a href="<?php echo site_url(); ?>/my-tickets/">View My Tickets</a></p>
       </div>
 	  </div>
+
+	   </div>
       </div>   <!-- # outer-wrapper-->
-    </div> <!-- #main content --> 
+    </div> <!-- #main content -->
     
     <?php
     
